aa910445099c176bacf92e5e697a15a841d45722
ZzZzZzZXCoo <77143312+ZzZzZzZXCoo@users.noreply.github.com>
2021-06-25 18:03:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ wget -q -O wgg "https://raw.githubusercontent.com/ZzZzZzZXCoo/error/main/wgg.sh"
 wget -q -O xray "https://raw.githubusercontent.com/ZzZzZzZXCoo/error/main/xray.sh"
 wget -q -O ssrss "https://raw.githubusercontent.com/ZzZzZzZXCoo/error/main/ssrss.sh"
 wget -q -O allvpn "https://raw.githubusercontent.com/ZzZzZzZXCoo/error/main/allvpn.sh"
-chmod +x menu
-chmod +x shsh
-chmod +x wgg
-chmod +x xray
-chmod +x ssrss
-chmod +x allvpn
+chmod +x menu && sed -i -e 's/\r$//' menu
+chmod +x shsh &&  sed -i -e 's/\r$//' shsh
+chmod +x wgg && sed -i -e 's/\r$//' wgg
+chmod +x xray && sed -i -e 's/\r$//' xray
+chmod +x ssrss && sed -i -e 's/\r$//' ssrss
+chmod +x allvpn && sed -i -e 's/\r$//' allvpn
