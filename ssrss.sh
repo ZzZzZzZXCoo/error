49dc79a03dc1f24ea5fc6dfe33faad4520757316
ZzZzZzZXCoo <77143312+ZzZzZzZXCoo@users.noreply.github.com>
2021-06-26 02:22:28 +0700

    Update ssrss.sh

--- ssrss.sh
+++ ssrss.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
+red="\e[1;31m"
+gren="\e[1;32m"
+blue="\e[1;34m"
+yelow="\e[1;33m"
+cyan="\e[1;36m"
+white="\e[1;37m"
 clear
+echo -e "════════════════════════════════" | lolcat
+echo -e "$gren═══════$white[SSR & SS Account]$gren════════"
+echo -e "════════════════════════════════" | lolcat
+echo -e "══════[SSR Account]═════" | lolcat
+echo -e "$red 1. $white Create SSR Account"
+echo -e "$red 2. $white Deleting SSR Account"
+echo -e "$red 3. $white Renew SSR Account Active"
+echo -e "$red 4. $white Show Other SSR Menu"
+echo -e "══════[SS Account]══════" | lolcat
+echo -e "$red 5. $white Create Shadowsocks Account"
+echo -e "$red 6. $white Delete Shadowsocks Account"
+echo -e "$red 7. $white Renew Shadowsocks Account"
+echo -e "$red 8. $white Check User Login Shadowsocks"
 echo -e " ════════════════════════════════" | lolcat
-echo -e " ═══════[SSR & SS Account]════════"
-echo -e " ════════════════════════════════" | lolcat
-echo -e " ══════[SSR Account]═════" | lolcat
-echo -e " 1.  Create SSR Account"
-echo -e " 2.  Deleting SSR Account"
-echo -e " 3.  Renew SSR Account Active"
-echo -e " 4.  Show Other SSR Menu"
-echo -e " ══════[SS Account]══════" | lolcat
-echo -e " 5.  Create Shadowsocks Account"
-echo -e " 6.  Delete Shadowsocks Account"
-echo -e " 7.  Renew Shadowsocks Account"
-echo -e " 8.  Check User Login Shadowsocks"
-echo -e " ════════════════════════════════" | lolcat
-echo -e " x.  Kembali ke Menu Utama"
+echo -e " $white  Enter Kembali ke Menu Utama"
 echo -e " ════════════════════════════════" | lolcat
 echo -e ""
 read -p "     Please Input Number  [1-8 or x] :  " ssrss
